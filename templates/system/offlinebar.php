accab5b26db18dce3eb3a1c6e44b43a6ff599406
doctorgrif <artem.grafov@gmail.com>
2013-10-30 07:46:46 +0600

    Change DOCTYPE Declarations

--- templates/system/offlinebar.php
+++ templates/system/offlinebar.php
@@ -29,8 +29,8 @@ $iso = explode('=', _ISO);
 // xml prolog
 echo '<?xml version="1.0" encoding="' . $iso[1] . '"?' . '>';
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
 	<style>
 		table.moswarning {
